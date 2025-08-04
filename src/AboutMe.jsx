da0a1bf44b63162d4594ef82dff0943173e377e8
pratyansh <pratyanshkpathak@gmail.com>
2025-08-04 17:29:40 +0530

    Updates about me sections

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -10,9 +10,9 @@ export default function AboutMe() {
                 <img src={MyPic} alt="A picture of me" /></div>
             <div>
                 <h1>About me:-</h1>
-                <p>Hello! my name is Pratyansh Pathak a 2nd year B.Tech student aspiring to be a Software Engineer in top tech firms. Driven by a passion for problem-solving
-and armed with solid computer science fundamentals, I aim to deliver innovative solutions.</p>
-                <p>I am currently looking for an internship in top tech companies . I am also currently learning competetitive programming and participating in Codeforces competitions.</p>
+                <p>Hello! my name is Pratyansh Pathak a 4th year B.Tech student aspiring to be a Software Engineer in top tech firms. Driven by a passion for problem-solving
+and armed with solid computer science fundamentals, I aim to deliver innovative and scalable solutions.</p>
+                <p>I am currently looking for full time opportunity in top tech companies. I am also into competetitive programming, currently I am a specialist in codeforces.</p>
                 <p>My hobbies include reading books, playing chess and watching movies.</p>
             </div>
             
